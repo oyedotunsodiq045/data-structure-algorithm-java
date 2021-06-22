57cd3acbbe292fe7ca0b24f592017f3761d2b26a
Sodiq <oyedotunsodiq045@gmail.com>
2021-06-21 22:33:59 -0500

    Feat: Selection Sort

--- src/stark/learndatastructureandalgorithm/selectionsort/Main.java
+++ src/stark/learndatastructureandalgorithm/selectionsort/Main.java
@@ -6,21 +6,14 @@ public class Main {
 
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-
+        for ( int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex-- ) {
             int largest = 0;
 
-            for (int i = 1; i <= lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[largest]) {
-                    largest = i;
-                }
-            }
+            for ( int i = 1; i <= lastUnsortedIndex; i++ )
+                if ( intArray[i] > intArray[largest] ) largest = i;
 
-            swap(intArray, largest, lastUnsortedIndex);
+            swap ( intArray, largest, lastUnsortedIndex );
         }
-
-        for (int i = 0; i < intArray.length; i++)
-            System.out.println(intArray[i]);
     }
 
     public static void swap(int[] array, int i, int j) {
