861ac54e445e436c12b721b8427e4553868163c3
Sodiq <oyedotunsodiq045@gmail.com>
2021-06-21 22:34:39 -0500

    Feat: Selection Sort

--- src/stark/learndatastructureandalgorithm/selectionsort/Main.java
+++ src/stark/learndatastructureandalgorithm/selectionsort/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
         for ( int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex-- ) {
+
             int largest = 0;
 
             for ( int i = 1; i <= lastUnsortedIndex; i++ )
